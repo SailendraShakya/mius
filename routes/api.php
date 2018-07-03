50a8a79f6e1be6ab55e83cf66af04c63e5f23b9f
8sq <sailendra.shakya@gmail.com>
2018-07-03 17:48:26 +0545

    changes in route

--- routes/api.php
+++ routes/api.php
@@ -24,6 +24,7 @@ Route::group(['prefix' => 'auth', 'middleware' => 'jwt.auth'], function () {
 
 Route::group(['prefix' => 'profile', 'middleware' => 'jwt.auth'], function () {
     Route::post('uploadImage', 'Api\ProfileController@uploadImage');
+    
     Route::post('updateGoal', 'Api\ProfileController@updateGoal');
 });
 
